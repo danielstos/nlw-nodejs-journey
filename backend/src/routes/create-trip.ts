23f6d7abdeb19a87a3e8dc39134d93bc3e75c1e9
danielstos <daniel.jose.stos@gmail.com>
2024-07-10 11:52:32 -0300

    feat: create participant table inclusion of participant in the database

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -4,8 +4,9 @@ import dayjs from "dayjs";
 import nodemailer from "nodemailer";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
-import { getMailClient } from "./mail";
+import { getMailClient } from "../lib/mail";
 
+// Definição do schema de validação para a rota de criação de viagem
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     "/trips",
@@ -20,26 +21,38 @@ export async function createTrip(app: FastifyInstance) {
         }),
       },
     },
+    // Validação e criação da viagem no banco de dados
+    //e envio de email para o proprietário da viagem
     async (request) => {
-      const { destination, starts_at, ends_at, owner_name, owner_email } =
-        request.body;
+      const { destination, starts_at, ends_at, owner_name, owner_email } = request.body
       if (dayjs(starts_at).isBefore(new Date())) {
-        throw new Error("Invalid trip start date ");
+        throw new Error("Invalid trip start date ")
       }
 
       if (dayjs(ends_at).isBefore(dayjs(starts_at))) {
-        throw new Error("Invalid trip end date ");
+        throw new Error("Invalid trip end date ")
       }
 
+      // Criando a viagem e a inclusão do participante  no banco de dados
       const trip = await prisma.trip.create({
         data: {
           destination,
           starts_at,
           ends_at,
-        },
-      });
+          participants: {
+            create: {
+              name: owner_name,
+              email: owner_email,
+              is_owner: true,
+              is_confirmed: true,
+            }
+          }
+        }
+      })
 
+      // Envio de email para o proprietário da viagem
       const mail = await getMailClient();
+
       const message = await mail.sendMail({
         from: {
           name: "Equipe plann.er",

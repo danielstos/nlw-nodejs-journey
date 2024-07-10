a2fbe269a4c5b047f01308dd532089af66573a9e
danielstos <daniel.jose.stos@gmail.com>
2024-07-10 00:28:28 -0300

    feat: create the createTrip route and date validation with dayjs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,27 @@
 import fastify from "fastify";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.get('/teste', () => {
-  return   "Hello, NLW!" 
-})
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("Server running");
-});
+  console.log("🔥 Server is running");
+})
+
+/*
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log("🔥 Server is running");
+  });
+  */
+
+  

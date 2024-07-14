ec098c6294b495d58f5c857c5b07eaff69f7555a
danielstos <daniel.jose.stos@gmail.com>
2024-07-14 15:15:48 -0300

    feat: cerate create-activity

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import {
 } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
 
 
 const app = fastify();
@@ -23,6 +24,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipants);
+app.register(createActivity);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("🔥 Server is running");

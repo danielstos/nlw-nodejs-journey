cf8c52dbfbb2817b980bc2521d91b770ec2b9bdd
danielstos <daniel.jose.stos@gmail.com>
2024-07-16 00:48:35 -0300

    feat:  created the routes createLink route creates links and getlinks link list route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,6 +9,8 @@ import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 
 const app = fastify();
@@ -27,6 +29,9 @@ app.register(confirmTrip);
 app.register(confirmParticipants);
 app.register(createActivity);
 app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+
 
 app.listen({ port: 3333 }).then(() => {
   console.log("🔥 Server is running");
